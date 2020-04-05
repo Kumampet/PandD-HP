1ab4f99f2993eb0f31b948f18f66c4ccfa8ea153
Kumampet <kumampet@gmail.com>
2020-04-06 03:35:30 +0900

    compreate 新入生向けモーダルウィンドウ

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,5 @@
-function modal_close() {
-    $("modal").animate({
-        opacity: 0,
-    }, 2000, 'swing', function () {
-        $(this).css('display','none');
-    });
-} 
+$(function () {
+    $("#modal").on('click', function () {
+        $("#modal").fadeOut(1000);
+    })
+})
